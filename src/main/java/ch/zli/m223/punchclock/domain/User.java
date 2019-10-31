b2d1ccd0304d30314c9e7c52a33a2c53572771ab
PanikSchale2001 <jeriel.frei@gmail.com>
2019-10-31 15:05:05 +0100

    Bugfixes

--- src/main/java/ch/zli/m223/punchclock/domain/User.java
+++ src/main/java/ch/zli/m223/punchclock/domain/User.java
@@ -1,7 +1,47 @@
 package ch.zli.m223.punchclock.domain;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String Password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
 }
